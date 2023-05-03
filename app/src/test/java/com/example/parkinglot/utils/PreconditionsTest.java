684e401ce246a42a3853fb16dce00928e6971e81
111598086 <t111598086@ntut.org.tw>
2023-05-03 18:14:04 +0800

    Add PreconditionsTest

---
+++ app/src/test/java/com/example/parkinglot/utils/PreconditionsTest.java
@@ -0,0 +1,58 @@
+package com.example.parkinglot.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PreconditionsTest {
+    boolean expressionTrue;
+    Object errorMessageTrue;
+    boolean expressionFalse;
+    Object errorMessageFalse;
+    String referenceNotNull;
+    Object errorMessageNotNull;
+    String referenceNull;
+    Object errorMessageNull;
+    @Before
+    public void setUp() throws Exception {
+        expressionTrue = true;
+        errorMessageTrue = "This should not be thrown";
+        expressionFalse = false;
+        errorMessageFalse = "This should be thrown";
+        referenceNotNull = "not null";
+        errorMessageNotNull = "This should not be thrown";
+        referenceNull = null;
+        errorMessageNull = "This should be thrown";
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void checkArgumentTest() {
+        // Test when expression is true
+        Preconditions.checkArgument(expressionTrue, errorMessageTrue); // No exception should be thrown
+        // Test when expression is false
+        try {
+            Preconditions.checkArgument(expressionFalse, errorMessageFalse);
+            fail("IllegalArgumentException should have been thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals(errorMessageFalse, e.getMessage());
+        }
+    }
+    @Test
+    public void checkNotNullTest() {
+        // Test when reference is not null
+        assertEquals(referenceNotNull, Preconditions.checkNotNull(referenceNotNull, errorMessageNotNull));
+        // Test when reference is null
+        try {
+            Preconditions.checkNotNull(referenceNull, errorMessageNull);
+            fail("NullPointerException should have been thrown");
+        } catch (NullPointerException e) {
+            assertEquals(errorMessageNull, e.getMessage());
+        }
+    }
+}
