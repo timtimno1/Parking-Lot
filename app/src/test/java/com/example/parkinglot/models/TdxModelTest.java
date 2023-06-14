dbe8eab53be92d12fa8f4f3cfe389c45d2a7b51c
Tim-selab <ligi.info73@yahoo.com>
2023-06-14 14:11:54 +0800

    Refactor: CityPhonePrefix class to add new methods for matching phone numbers with different formats and update autoAddRegionNumber method to handle these new formats. Also, update TdxModelTest to test the new functionality.

--- app/src/test/java/com/example/parkinglot/models/TdxModelTest.java
+++ app/src/test/java/com/example/parkinglot/models/TdxModelTest.java
@@ -4,6 +4,8 @@ import static com.example.parkinglot.utils.CityPhonePrefix.autoAddRegionNumber;
 import static com.example.parkinglot.utils.CityPhonePrefix.getCityPhonePrefix;
 import static org.junit.Assert.*;
 
+import com.example.parkinglot.utils.Cities;
+import com.example.parkinglot.utils.CityPhonePrefix;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -57,10 +59,13 @@ public class TdxModelTest {
         String legalNumberWithoutRegion1 = "4989295";
         String legalNumberWithoutRegion2 = "12345678";
         assertEquals("0928098455", autoAddRegionNumber(cellphone, null));
-        assertEquals("03-4989295", autoAddRegionNumber(legalNumberWithRegion1, "Taoyuan"));
-        assertEquals("024989295", autoAddRegionNumber(legalNumberWithRegion2, "Taoyuan"));
-        assertEquals("034989295", autoAddRegionNumber(legalNumberWithoutRegion1, "Taoyuan"));
-        assertEquals("0312345678", autoAddRegionNumber(legalNumberWithoutRegion2, "Taoyuan"));
+        assertEquals("03 - 4989295", autoAddRegionNumber(legalNumberWithRegion1, "Taoyuan"));
+        assertEquals("02 - 4989295", autoAddRegionNumber(legalNumberWithRegion2, "Taipei"));
+        assertEquals("03 - 4989295", autoAddRegionNumber(legalNumberWithoutRegion1, "Taoyuan"));
+        assertEquals("03 - 12345678", autoAddRegionNumber(legalNumberWithoutRegion2, "Taoyuan"));
+        for(Cities city : Cities.values()) {
+            assertEquals(CityPhonePrefix.getCityPhonePrefix(city.toString()) + " - 4989295", autoAddRegionNumber(legalNumberWithoutRegion1, city.toString()));
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
